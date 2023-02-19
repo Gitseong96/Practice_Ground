fe46eee3a877bf50a6dad0f1e54ab7ebcee0bffc
Gitseong96 <wkdrl523@gmail.com>
2023-02-19 20:43:52 +0900

    fix

--- algorithm/Big_O/example1.js
+++ algorithm/Big_O/example1.js
@@ -19,15 +19,12 @@ function sameFrequency(num1, num2) {
         return true
     }
 }
-
 sameFrequency(182, 281) // true
 sameFrequency(34, 14) // false
 sameFrequency(3589578, 5879385) // true
 sameFrequency(22, 222) // false
 
-
 // todo:변수 개수의 인수를 허용하고 전달된 인수 중에 중복된 인수가 있는지 확인하는, areThereDuplicates라는 함수를 구현합니다
-
 
 function areThereDuplicates(...args) {
     args.sort((a, b) => a > b);
