35c49e3d36c7511d0eb85906426070cd16880f14
Gitseong96 <wkdrl523@gmail.com>
2023-02-27 21:23:33 +0900

    fix

--- algorithm/codeing/first.js
+++ algorithm/codeing/first.js
@@ -35,7 +35,6 @@ function solution(phone_number) {
 
 function solution(absolutes, signs) {
     var answer = 0;
-    let result = absolutes.concat(signs)
     for (let i = 0; i < absolutes.length; i++) {
         if (signs[i] === true) {
             answer = answer + absolutes[i]
